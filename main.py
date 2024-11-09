4a4e05fb9de110ed3bfaecc6ff98296ebada4316
ThePrimeagen <the.primeagen@aol.com>
2024-11-10 02:21:40 +0300

    added main function

--- main.py
+++ main.py
@@ -5,13 +5,22 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
+    clock = pygame.time.Clock()
+    dt = 0
+    ##
+
     while True:
+        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill("black")
         pygame.display.flip()
 
+        dt = clock.tick(60) / 1000
+        print(dt)
 
 if __name__ == "__main__":
     main()
